876204788ff42daa02564e1edb600af5cd823d02
genial <colorfishes@126.com>
2015-08-24 13:50:49 +0800

    v1.0.0

--- .exvim.pyblog/update-idutils.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export DEST="./.exvim.pyblog"
-export TOOLS="/home/genial/.exvim//vimfiles/tools/"
-export TMP="${DEST}/_ID"
-export TARGET="${DEST}/ID"
-sh ${TOOLS}/shell/bash/update-idutils.sh
--- .exvim.pyblog/update-inherits.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export DEST="./.exvim.pyblog"
-export TOOLS="/home/genial/.exvim//vimfiles/tools/"
-export TMP="${DEST}/_inherits"
-export TARGET="${DEST}/inherits"
-sh ${TOOLS}/shell/bash/update-inherits.sh
