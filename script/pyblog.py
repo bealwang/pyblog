4eb1f3929bddf2980f0a31633f1b7735229dbf60
genial <colorfishes@126.com>
2015-08-24 19:37:56 +0800

    v1.0.2

--- script/pyblog.py
+++ script/pyblog.py
@@ -48,7 +48,10 @@ def render_template(template_name, args):
     env = init_jinja2()
     html = env.get_template(template_name+'.html').render(**args)
     try:
-        _pwd = '../www/html/'+template_name+'.html'
+        if 'index' == template_name:
+            _pwd = '../www/'+template_name+'.html'
+        else:
+            _pwd = '../www/html/blog/'+template_name+'.html'
         _dir = os.path.dirname(_pwd)
         if not os.path.exists(_dir):
             os.makedirs(_dir)
@@ -60,5 +63,5 @@ def render_template(template_name, args):
 
 if __name__ == '__main__':
     args = dict(foo = get_file())
-    render_template('blogs', args)
     render_template('blog', args)
+    render_template('index', args)
