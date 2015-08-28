1754cb399fcc9787fa329965e7daf94e8f6034aa
genial <colorfishes@126.com>
2015-08-28 21:04:13 +0800

    do something

--- www/static/js/genialwang.js
+++ www/static/js/genialwang.js
@@ -1,8 +1,3 @@
-/**
-* JavaScript脚本实现回到页面顶部示例
-* @param acceleration 速度
-* @param stime 时间间隔 (毫秒)
-**/
 function gotoTop(acceleration,stime) {
    acceleration = acceleration || 0.1;
    stime = stime || 10;
@@ -23,19 +18,16 @@ function gotoTop(acceleration,stime) {
    var x3 = window.scrollX || 0;
    var y3 = window.scrollY || 0;
  
-   // 滚动条到页面顶部的水平距离
    var x = Math.max(x1, Math.max(x2, x3));
-   // 滚动条到页面顶部的垂直距离
    var y = Math.max(y1, Math.max(y2, y3));
  
-   // 滚动距离 = 目前距离 / 速度, 因为距离原来越小, 速度是大于 1 的数, 所以滚动距离会越来越小
    var speeding = 1 + acceleration;
    window.scrollTo(Math.floor(x / speeding), Math.floor(y / speeding));
  
-   // 如果距离不为零, 继续调用函数
    if(x > 0 || y > 0) {
        var run = "gotoTop(" + acceleration + ", " + stime + ")";
        window.setTimeout(run, stime);
    }
 }
-//素材家园 - www.sucaijiayuan.com
+
+
